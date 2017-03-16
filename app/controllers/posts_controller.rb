6b889562dd14404e45a61d0995a0c5f2ecb01111
张伟 <zhangwei19890518@gmail.com>
2017-03-16 21:07:52 +0800

    complete the extra homework

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,11 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
   def create
     @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
@@ -17,6 +22,32 @@ class PostsController < ApplicationController
       render :new
     end
   end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if current_user != @post.user
+      redirect_to account_posts_path, alert: "你没有编辑权限！"
+    end
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "编辑成功！"
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if current_user == @post.user
+      @post.destroy
+      redirect_to account_posts_path, alert: "删除文章成功！"
+    else
+      redirect_to account_posts_path, alert: "你没有相应权限！"
+    end
+  end
+
 
   private
 
